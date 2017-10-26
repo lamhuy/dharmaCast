6c23c6ded76944a3f8038f15c9791b82a6f1c155
lamhuy <lamhuy@vt.edu>
2017-10-25 23:34:42 -0400

    using footer controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -140,9 +140,9 @@ angular.module('starter.controllers', ['starter.factories'])
 		$('.player .title').text(track.title);
 		$('.player .artist').text(track.artist);
 		// $('.player .cover').css('background-image','url(/' + cover+')');;
-
+		$scope.buttonText = 'Play';
 		song = new Audio(track.src);
-		//console.info("SONG DURATION: ", song.duration);
+		
 		// timeupdate event listener
 		song.addEventListener('timeupdate', function() {
 			var curtime = parseInt(song.currentTime, 10);
@@ -154,7 +154,7 @@ angular.module('starter.controllers', ['starter.factories'])
 				  s = '0' + s;
 			$scope.duration =  m + ':' + s;
 			
-			$scope.value = song.currentTime;
+			$scope.barValue = song.currentTime;
 			$scope.$apply()
 			
 		});
@@ -170,10 +170,10 @@ angular.module('starter.controllers', ['starter.factories'])
 		}
 		
 		song.addEventListener("loadeddata", function() {
-			 console.log("Audio data loaded");
-			 console.log("Audio duration: " + this.duration);
+			console.log("Audio data loaded");
+			console.log("Audio duration: " + this.duration);
 			CreateSeekBar();
-			});
+		});
 	
 	
 		if(currentTrackIndex == 0){
@@ -187,131 +187,41 @@ angular.module('starter.controllers', ['starter.factories'])
 
 	function playAudio() {
 		song.play();
-
 		tracker.slider("option", "max", song.duration);
-
-		$('.play').addClass('hidden');
-		$('.pause').addClass('visible');
 	}
 	function stopAudio() {
 		song.pause();
-
-		$('.play').removeClass('hidden');
-		$('.pause').removeClass('visible');
 	}
 
-	// play click
-	$('.play').click(function(e) {
-		e.preventDefault();
-
-		playAudio();
-	});
-
-	// pause click
-	$('.pause').click(function(e) {
-		e.preventDefault();
-
-		stopAudio();
-	});
-
-	// forward click
-/*	$('.fwd').click(function(e) {
-		e.preventDefault();
-
-		stopAudio();
-
-		var next = $('.playlist li.active').next();
-		if (next.length == 0) {
-			next = $('.playlist li:first-child');
-		}
-		initAudio(next);
-	});
-
-	// rewind click
-	$('.rew').click(function(e) {
-		e.preventDefault();
-
-		stopAudio();
-
-		var prev = $('.playlist li.active').prev();
-		if (prev.length == 0) {
-			prev = $('.playlist li:last-child');
-		}
-		initAudio(prev);
-	});*/
-
 	$scope.prevTrack = function() {
-
 		stopAudio();
-				
 		if(currentTrackIndex > 0){			
 			currentTrackIndex--;
+			$scope.currentTrackIndex =currentTrackIndex;			
 			initTrack(tracks[currentTrackIndex]);
 		}
-
 		
 	}
 
 	$scope.nextTrack = function() {
-
-		stopAudio();
-		
+		stopAudio();		
 		if(currentTrackIndex < tracks.length){
 			currentTrackIndex++;
+			$scope.currentTrackIndex =currentTrackIndex;			
 			initTrack(tracks[currentTrackIndex]);
 		}
-
-
-
 	}
-
-	// show playlist
-	/*
-	 * $('.pl').click(function (e) { e.preventDefault();
-	 * 
-	 * $('.playlist').fadeIn(300); });
-	 */
-
-	// playlist elements - click
-	/*
-	 * $('.playlist li').click(function () { stopAudio(); initAudio($(this));
-	 * playAudio(); });
-	 */
-	// initialization - first element in playlist
-	// initAudio($('.playlist li:first-child'));
-	//initTrack(tracks[currentTrackIndex]);
-	// set volume
-	//song.volume = 0.8;
-
-	// initialize the volume slider
-	volume.slider({
-		range : 'min',
-		min : 1,
-		max : 100,
-		value : 80,
-		start : function(event, ui) {
-		},
-		slide : function(event, ui) {
-			song.volume = ui.value / 100;
-		},
-		stop : function(event, ui) {
-		},
-	});
-
-	// empty tracker slider
-	tracker.slider({
-		range : 'min',
-		min : 0,
-		max : 10,
-		start : function(event, ui) {
-		},
-		slide : function(event, ui) {
-			song.currentTime = ui.value;
-		},
-		stop : function(event, ui) {
+	
+	$scope.playBtn = function()	{
+		if($scope.buttonText == 'Play'){
+			playAudio();
+			$scope.buttonText = 'Pause';
+		}else{
+			stopAudio();			
+			$scope.buttonText = 'Play';
 		}
-	});
-
+	}
+	
 	$scope.playTrack = function(event, index, track) {
 		event.preventDefault();
 		stopAudio();
@@ -324,19 +234,19 @@ angular.module('starter.controllers', ['starter.factories'])
 			playAudio();
 		}, 100);
 
+		$scope.buttonText = 'Pause';
+		
 	}
 	
 	
 	function CreateSeekBar() {
-		  var seekbar = document.getElementById("audioSeekBar");
-		    $scope.value = 0;
-		    $scope.min = 0;
-		    $scope.max = song.duration;;
-		    $scope.duration = '0:00';
+		 
+		 $scope.barValue = 0;
+		 $scope.barMin = 0;
+		 $scope.barMax = song.duration;;
+		 $scope.duration = '0:00';
 		    
-		  seekbar.min = 0;
-		  seekbar.max = 
-		  seekbar.value = 0;
+		 
 		  var s = parseInt(song.duration % 60);
 		  var m = parseInt((song.duration / 60) % 60);
 		  if(s<10)
@@ -346,23 +256,11 @@ angular.module('starter.controllers', ['starter.factories'])
 		  $scope.$apply()
 	}
 
-/*	function EndofAudio() {
-	  document.getElementById("audioSeekBar").value = 0;
-	}*/
-
-	$scope.audioSeekBar = function() {
-	  var seekbar = document.getElementById("audioSeekBar");
-	  song.currentTime = seekbar.value;
+	$scope.audioSeekBar = function() {	  
+	  song.currentTime = $scope.barValue;
 	}
 
 
-/*	audio.addEventListener("timeupdate", function() {
-	  var duration = document.getElementById("duration");
-	  var s = parseInt(song.currentTime % 60);
-	  var m = parseInt((song.currentTime / 60) % 60);
-	  duration.innerHTML = m + ':' + s;
-	}, false);
-	*/
 	
 	$scope.$on("$destroy", function() {
 		console.info("leaving controller");
